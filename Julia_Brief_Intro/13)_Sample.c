3e0aa6bed4828c2997f70b1e20aab28aee284e49
Amiya Das <2ce01057@iitbbs.ac.in>
2025-08-27 09:41:07 +0530

    Learning Files Of Julia

---
+++ Julia_Brief_Intro/13)_Sample.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    for (i = 1; i <= 9; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+    return 0;
+}
+
+
+
+//      gcc 13)_Sample.c -o 13)_Sample
+//      ./13)_Sample
